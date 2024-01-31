68308049d808c6e94f78da8aab09a673ac6e0f04
Vikramop <veervikramsingh132@gmail.com>
2024-01-31 09:26:31 +0530

    Inception

---
+++ 01_Inception/App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement(
+  'h1',
+  { id: 'hello', style: { color: 'red' } }, // now this {} is used to pass attribute to the element.
+  'Hello from React'
+);
+
+// so what is heading?
+// heading is an object of react which is creating element.
+// and the hello from react is children part
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+// so the render is method which basically takes the object convert to html element and put ir on the DOM.
