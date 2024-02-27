7bfc8275f8afa0f1b0308077754d8f58edfedd87
Vikramop <veervikramsingh132@gmail.com>
2024-02-27 10:16:44 +0530

    completed 2 parts of ep6

--- 04_Making-an-app/src/components/Body.js
+++ 04_Making-an-app/src/components/Body.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RestaurantCard from './RestaurantCard';
 import { resList } from '../utils/constants';
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/mapi/homepage/getCards?lat=12.9351929&lng=77.62448069999999'
+    );
+
+    const json = await data.json();
+
+    console.log(json.data.restaurants);
+  };
 
   return (
     <div className="body">
