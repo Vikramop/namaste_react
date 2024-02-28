4c0e0d2f54b337d68edd27adef6c7a73f635ce73
Vikramop <veervikramsingh132@gmail.com>
2024-02-28 22:40:15 +0530

    api call from swiggy.

--- 04_Making-an-app/src/components/Body.js
+++ 04_Making-an-app/src/components/Body.js
@@ -11,12 +11,17 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://www.swiggy.com/mapi/homepage/getCards?lat=12.9351929&lng=77.62448069999999'
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
     );
 
     const json = await data.json();
-
-    console.log(json.data.restaurants);
+    console.log(json);
+    setListOfRes(
+      json.data.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(
+      json.data.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
   return (
