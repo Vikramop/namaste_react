1628a4b7d82c1e01c8ab2c39b6c6ab452c5c0db4
Vikramop <veervikramsingh132@gmail.com>
2024-03-04 20:25:02 +0530

    swiggy api resolve

--- 04_Making-an-app/src/components/Body.js
+++ 04_Making-an-app/src/components/Body.js
@@ -15,19 +15,23 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+      'https://www.swiggy.com/mapi/homepage/getCards?lat=12.9351929&lng=77.62448069999999'
+      // 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
     );
 
     const json = await data.json();
     console.log(json);
     setListOfRes(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json.data.success.cards[1].gridWidget.gridElements.infoWithStyle
+        .restaurants
     );
     setFilterdRes(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json.data.success.cards[1].gridWidget.gridElements.infoWithStyle
+        .restaurants
     );
     console.log(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json.data.success.cards[1].gridWidget.gridElements.infoWithStyle
+        .restaurants
     );
   };
 
